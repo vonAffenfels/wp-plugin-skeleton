7b09474da73e2962703edb806d5338ac095c1386
Christoph Friedrich <christoph.friedrich@vonaffenfels.de>
2023-08-30 08:15:43 +0200

    Updated framework

--- vendor_prefixed/vonaffenfels/vaf-wp-framework/src/Composer/PluginSkeletonAction.php
+++ vendor_prefixed/vonaffenfels/vaf-wp-framework/src/Composer/PluginSkeletonAction.php
@@ -8,6 +8,7 @@ use WPPluginSkeleton_Vendor\Composer\Pcre\Preg;
 use WPPluginSkeleton_Vendor\Composer\Script\Event;
 use Exception;
 use InvalidArgumentException;
+use WPPluginSkeleton_Vendor\Seld\JsonLint\ParsingException;
 use WPPluginSkeleton_Vendor\Symfony\Component\Process\ExecutableFinder;
 use WPPluginSkeleton_Vendor\Symfony\Component\Process\Process;
 class PluginSkeletonAction
@@ -40,12 +41,144 @@ class PluginSkeletonAction
         }
         return \false !== \filter_var($email, \FILTER_VALIDATE_EMAIL);
     }
-    private static function patchFile(string $file, array $replacements) : bool
+    /**
+     * @throws ParsingException
+     * @throws Exception
+     */
+    private static function patchComposerJson(string $slug, string $pluginDescription, string $namespace, string $authorName, string $authorEmail, string $website) : void
     {
-        $content = \file_get_contents($file);
-        $content = \str_replace(\array_keys($replacements), \array_values($replacements), $content);
-        \file_put_contents($file, $content);
-        return \true;
+        $file = Factory::getComposerFile();
+        $composerJson = new JsonFile($file);
+        $composerData = $composerJson->read();
+        // Remove post-create-project-cmd script
+        unset($composerData['scripts']['post-create-project-cmd']);
+        // Add build container command
+        $composerData['scripts']['build-container'] = [$namespace . '\\Plugin::buildContainer'];
+        $authorData = [];
+        if (!empty($authorName)) {
+            $authorData['name'] = $authorName;
+        }
+        if (!empty($authorEmail)) {
+            $authorData['email'] = $authorEmail;
+        }
+        if (!empty($website)) {
+            $authorData['homepage'] = $website;
+        }
+        $composerData['name'] = 'vonaffenfels/' . $slug;
+        $composerData['type'] = 'wordpress-plugin';
+        if (!empty($pluginDescription)) {
+            $composerData['description'] = $pluginDescription;
+        } else {
+            unset($composerData['description']);
+        }
+        if (!empty($website)) {
+            $composerData['homepage'] = $website;
+        } else {
+            unset($composerData['homepage']);
+        }
+        if (!empty($authorData)) {
+            $composerData['authors'] = [$authorData];
+        } else {
+            unset($composerData['authors']);
+        }
+        $composerJson->write($composerData);
+    }
+    private static function createConfigServicesYaml(string $namespace) : void
+    {
+        $content = <<<END
+services:
+  _defaults:
+    autowire: true
+    autoconfigure: true
+
+  {$namespace}\\:
+    resource: '../src/'
+    exclude:
+      - '../src/Plugin.php'
+END;
+        \file_put_contents(\realpath('./config/services.yaml'), $content);
+    }
+    private static function createSrcPluginPhp(string $namespace, string $vendorNamespace) : void
+    {
+        $content = <<<END
+<?php
+
+namespace {$namespace};
+
+class Plugin extends \\{$vendorNamespace}\\VAF\\WP\\Framework\\Plugin
+{
+}
+
+END;
+        \file_put_contents(\realpath('./src/Plugin.php'), $content);
+    }
+    private static function createScoperIncPhp(string $vendorNamespace) : void
+    {
+        $content = <<<END
+<?php
+
+use VAF\\WP\\Framework\\PHPScoperConfigGenerator;
+
+\$scoperConfigGen = new PHPScoperConfigGenerator(
+    baseDir: __DIR__,
+    prefix: '{$vendorNamespace}',
+    buildDir: './vendor_prefixed'
+);
+
+// Do not prefix ignored packages
+//\$scoperConfigGen->ignorePackage('myvendor/mypackage');
+
+return \$scoperConfigGen->buildConfig();
+
+END;
+        \file_put_contents(\realpath('./scoper.inc.php'), $content);
+    }
+    private static function createMainPluginFile(string $slug, string $pluginName, string $pluginDescription, string $namespace, string $authorName, string $authorEmail, string $website) : void
+    {
+        $pluginName = " * Plugin Name:       {$pluginName}";
+        if (!empty($pluginDescription)) {
+            $pluginDescription = " * Description:       {$pluginDescription}\n";
+        }
+        $authorComplete = '';
+        if (!empty($authorName) && !empty($authorEmail)) {
+            $authorComplete = \sprintf('%s <%s>', $authorName, $authorEmail);
+        } elseif (!empty($authorName)) {
+            $authorComplete = $authorName;
+        } elseif (!empty($authorEmail)) {
+            $authorComplete = $authorEmail;
+        }
+        if (!empty($authorComplete)) {
+            $authorComplete = " * Author:            {$authorComplete}\n";
+        }
+        if (!empty($website)) {
+            $website = " * Author URI:        {$website}\n";
+        }
+        $versionString = " * Version:           1.0.0\n";
+        $requiresString = " * Requires at least: 6.2\n";
+        $content = <<<END
+<?php
+
+/**
+{$pluginName}{$pluginDescription}{$authorComplete}{$website}{$versionString}{$requiresString}
+ */
+
+use {$namespace}\\Plugin;
+
+if (!defined('ABSPATH')) {
+    die('');
+}
+
+\$autoloadPath = __DIR__ . '/vendor/autoload.php';
+if (file_exists(\$autoloadPath)) {
+    require_once \$autoloadPath;
+}
+
+Plugin::registerPlugin(__FILE__, defined('WP_DEBUG') && WP_DEBUG);
+
+END;
+        \file_put_contents("./{$slug}.php", $content);
+        // Delete skeleton file
+        \unlink(\realpath('./wp-plugin-skeleton.php'));
     }
     /**
      * @throws Exception
@@ -88,61 +221,11 @@ class PluginSkeletonAction
             $io->writeError('To restart simply run "composer run-script post-create-project-cmd"');
             return;
         }
-        $authorComplete = '';
-        if (!empty($authorName) && !empty($authorEmail)) {
-            $authorComplete = \sprintf('%s <%s>', $authorName, $authorEmail);
-        } elseif (!empty($authorName)) {
-            $authorComplete = $authorName;
-        } elseif (!empty($authorEmail)) {
-            $authorComplete = $authorEmail;
-        }
-        $replacementArray = ['%%PLUGIN_NAME%%' => $pluginName, '%%PLUGIN_DESCRIPTION%%' => $pluginDescription, '%%PLUGIN_SLUG%%' => $slug, '%%PLUGIN_NAMESPACE%%' => $namespace, '%%VENDOR_NAMESPACE%%' => $vendorNamespace, '%%WEBSITE%%' => $website, '%%AUTHOR_COMPLETE%%' => $authorComplete, '%%AUTHOR_NAME%%' => $authorName, '%%AUTHOR_EMAIL%%' => $authorEmail];
-        $filesToPatch = ['config/services.yaml', 'src/Plugin.php', 'wp-plugin-skeleton.php', 'scoper.inc.php'];
-        foreach ($filesToPatch as $file) {
-            if (!self::patchFile(\realpath($cwd . '/' . $file), $replacementArray)) {
-                $io->writeError('<error>Error patching file "' . $file . '"!</error>');
-                return;
-            }
-        }
-        $file = Factory::getComposerFile();
-        $composerJson = new JsonFile($file);
-        $composerData = $composerJson->read();
-        // Remove post-create-project-cmd script
-        if (isset($composerData['scripts']['post-create-project-cmd'])) {
-            unset($composerData['scripts']['post-create-project-cmd']);
-        }
-        // Add build container command
-        $composerData['scripts']['build-container'] = [$namespace . '\\Plugin::buildContainer'];
-        // Add post-dump-autoload command
-        $composerData['scripts']['post-autoload-dump'] = ['WPPluginSkeleton_Vendor\\VAF\\WP\\Framework\\Composer\\PluginActions::prefixDependencies'];
-        $authorData = [];
-        if (!empty($authorName)) {
-            $authorData['name'] = $authorName;
-        }
-        if (!empty($authorEmail)) {
-            $authorData['email'] = $authorEmail;
-        }
-        if (!empty($website)) {
-            $authorData['homepage'] = $website;
-        }
-        $composerData['name'] = 'vonaffenfels/' . $slug;
-        $composerData['type'] = 'wordpress-plugin';
-        if (!empty($pluginDescription)) {
-            $composerData['description'] = $pluginDescription;
-        } else {
-            unset($composerData['description']);
-        }
-        if (!empty($website)) {
-            $composerData['homepage'] = $website;
-        } else {
-            unset($composerData['homepage']);
-        }
-        if (!empty($authorData)) {
-            $composerData['authors'] = [$authorData];
-        } else {
-            unset($composerData['authors']);
-        }
-        $composerJson->write($composerData);
-        $io->writeError(['<info>Finished</info>', 'Don\'t forget to check files and do last changes', 'Happy development']);
+        self::createConfigServicesYaml($namespace);
+        self::createSrcPluginPhp($namespace, $vendorNamespace);
+        self::createScoperIncPhp($vendorNamespace);
+        self::createMainPluginFile($slug, $pluginName, $pluginDescription, $namespace, $authorName, $authorEmail, $website);
+        self::patchComposerJson($slug, $pluginDescription, $namespace, $authorName, $authorEmail, $website);
+        $io->writeError(['', '<info>Finished</info>', 'Don\'t forget to check files and do last changes', 'Happy development']);
     }
 }
