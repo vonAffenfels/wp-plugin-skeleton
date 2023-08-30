f01c92dda4bfc0de48f8b2f6b978cde146f34d15
Christoph Friedrich <christoph.friedrich@vonaffenfels.de>
2023-08-30 09:42:31 +0200

    Updated framework

--- vendor_prefixed/vonaffenfels/vaf-wp-framework/src/Composer/PluginSkeletonAction.php
+++ vendor_prefixed/vonaffenfels/vaf-wp-framework/src/Composer/PluginSkeletonAction.php
@@ -226,10 +226,6 @@ END;
         self::createScoperIncPhp($vendorNamespace);
         self::createMainPluginFile($slug, $pluginName, $pluginDescription, $namespace, $authorName, $authorEmail, $website);
         self::patchComposerJson($slug, $pluginDescription, $namespace, $authorName, $authorEmail, $website);
-        $eventDispatcher = $event->getComposer()->getEventDispatcher();
-        $eventDispatcher->addListener('internal-skeleton', 'composer update');
-        $eventDispatcher->addListener('internal-skeleton', 'composer build-container');
-        $eventDispatcher->dispatch('internal-skeleton');
-        $io->writeError(['', '<info>Finished</info>', 'Don\'t forget to check files and do last changes', 'Happy development']);
+        $io->writeError(['', '<info>Finished</info>', 'Don\'t forget to check files and do last changes', 'Before using the plugin run <comment>composer update</comment> and <comment>composer build-container</comment>', 'Happy development']);
     }
 }
